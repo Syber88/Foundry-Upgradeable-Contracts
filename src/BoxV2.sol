e0ed2933337a14184c71b418fa8cac7fa34b53fa
Syber88 <siyamgz88@gmail.com>
2025-03-28 15:21:06 +0200

    added deployer scripts and added a proxy

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -2,24 +2,11 @@
 pragma solidity ^0.8.24;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
-import {Initializable} from "@openzeppelin/contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
 
-
-
-contract BoxV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+contract BoxV2 is UUPSUpgradeable {
     uint256 internal number;
 
-    ///@custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize() public initializer {
-        __Ownable_init(msg.sender);
-        __UUPSUpgradeable_init();
-    }
-
+    function setNumber() external {}
     function getNumber() external view returns (uint256) {
         return number;
     }
