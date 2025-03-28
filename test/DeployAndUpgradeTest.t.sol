060b7561125af0d173e1d6c3f0a6897c032c23dd
Syber88 <siyamgz88@gmail.com>
2025-03-28 20:33:27 +0200

    initialized deployer and upgrade test

---
+++ test/DeployAndUpgradeTest.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.24; 
+
+import {Test} from "forge-std/Test.sol";
+import {DeployBox} from "../script/DeployBox.s.sol";
+import {UpgradeBox} from "../script/UpgradeBox.s.sol";
+import {BoxV2} from "../src/BoxV2.sol";
+import {BoxV1} from "../src/BoxV1.sol";
+
+
+contract DeployAndUpgradeTest is Test{
+    DeployBox public deployer;
+    UpgradeBox public upgrader;
+    address public OWNER = makeAddr("owner");
+
+    address public proxy;
+
+    function setUp() public {
+        deployer = new DeployBox();
+        upgrader = new UpgradeBox();
+        proxy = deployer.run(); //points to boxV1
+
+
+    }
+    
+}
